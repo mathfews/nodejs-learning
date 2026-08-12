2727a9fbae98f91c27a80bc58513f41c0e043869
mathfews <matheusfcw1010@gmail.com>
2026-08-12 16:18:14 -0300

    refactor: restructure API with query parameters

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
 import http from "node:http"
-import {getDataFromDB} from "./scripts/db.js";
+import { getDataFromDB } from "./scripts/db.js"
 
 const port = 8000
 
+function filterData(data, queryParams) {
+  const { continent, is_open_to_public, country } = queryParams
+  if (continent) {
+    data = data.filter(entry => entry.continent.toLowerCase() == continent.toLowerCase())
+  }
+  if (is_open_to_public) {
+    const isOpen = is_open_to_public.toLowerCase() == "true"
+
+    data = data.filter(entry => entry.is_open_to_public == isOpen)
+  }
+  if (country) {
+    data = data.filter(entry => entry.country.toLowerCase() == country.toLowerCase())
+  }
+  return data
+}
+
 const server = http.createServer( async (req, res) => {
-    const data = await getDataFromDB()
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Content-type", "application/json")
-    if ((req.url) === "/api/") {
-        res.write(JSON.stringify(data))
+  const data = await getDataFromDB()
+  res.setHeader("Content-type", "application/json")
+  
+  const urlObj = new URL(req.url, `http://${req.headers.host}`)
+  const queryParams = Object.fromEntries(urlObj.searchParams)
+
+  if (urlObj.pathname == "/api" || req.url == "/api/") {
+    if (Object.keys(queryParams).length == 0) {
+      res.statusCode = 200
+      res.end(JSON.stringify({message: data}))
     }
-    else if ( (req.url).includes("/api/country/") ) {
-        const country = req.url.split("/")[3].toLowerCase()
-        const selected_country_data = data.filter(element => element.country.toLowerCase() === country)
-        if (selected_country_data.length === 0) {
-            res.end(JSON.stringify("404"))
-        }
-        else {
-            res.end(JSON.stringify(selected_country_data))
-        }
+    else {
+      res.statusCode = 200
+      res.end(JSON.stringify({ "data": filterData(data, queryParams) }))
     }
-    res.end()
+  }
+  
+  else {
+    res.statusCode = 404
+    res.end(JSON.stringify({ message: "404 not found" }))
+  }
 })
 
 server.listen(port, () => console.log(`running on: ${port}`))
