f177233497affdd9fc02dd1eaea22ee1ec024bd8
mathfews <matheusfcw1010@gmail.com>
2026-08-12 18:41:34 -0300

    feat: serve HTML files with Node.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hi")
+import http from 'node:http'
+import path from "node:path"
+import fs from "node:fs"
+
+const port = 8000
+
+const __dirname = import.meta.dirname
+
+const filepath = path.join(__dirname, "public", "index.html")
+
+const server = http.createServer((req, res) => {
+    const content = fs.readFileSync(filepath, 'utf8')
+    res.end(content)
+})
+
+server.listen(port, () => console.log(`running on: ${port}`))
