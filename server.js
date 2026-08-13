80ea2d684a63ab3de801c86743a2a219844ae02f
mathfews <matheusfcw1010@gmail.com>
2026-08-13 10:29:07 -0300

    feat: handle file not found and server errors

--- server.js
+++ server.js
@@ -16,8 +16,26 @@ const server = http.createServer( async (req, res) => {
     }
 
     const pathToResource = path.join(publicPath, req.url === "/" ? "index.html" : req.url)
-    const content = await fs.readFile(pathToResource)
-    res.end(content)
+
+
+    try {
+        const content = await fs.readFile(pathToResource)
+        res.end(content)
+    }
+    catch(error) {
+        if (error.code == "ENOENT") {
+            res.statusCode = 404
+            const pathTo404 = path.join(publicPath, "404.html")
+            const content = await fs.readFile(pathTo404)
+            res.end(content)
+        }
+        else {
+            res.statusCode = 500
+            res.setHeader("Content-type", "text/html")
+            res.end(`<html><h1>Server error: ${error.code}</h1></html>`)
+        }
+    }
+
 })
 
 server.listen(port, () => console.log(`running on: ${port}`))
